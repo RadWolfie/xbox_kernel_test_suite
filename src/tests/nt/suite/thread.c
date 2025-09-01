85acd67d68cf9d8fc39833d0f39959926c81b190
RadWolfie <RadWolfie@users.noreply.github.com>
2025-09-01 19:29:39 +0000

    debug: take 3 works better

--- src/tests/nt/suite/thread.c
+++ src/tests/nt/suite/thread.c
@@ -13,20 +13,18 @@ typedef struct {
     BOOL run;
     ULONG counter;
     HANDLE hEventThread;
-    HANDLE hMutex;
+    HANDLE hEventMain;
 } rs_thread_struct;
 
 static DWORD NTAPI NtResumeSuspendThread_thread(void* arg)
 {
-    BOOL test_passed = 1;
     rs_thread_struct* rs_thread = (rs_thread_struct*)arg;
     do {
         (void)WaitForSingleObject(rs_thread->hEventThread, INFINITE);
-        DWORD result_wait = WaitForSingleObject(rs_thread->hMutex, INFINITE);
-        GEN_CHECK(result_wait, WAIT_OBJECT_0, "result_wait (thread)");
         rs_thread->counter++;
-        (void)ReleaseMutex(rs_thread->hMutex);
 
+        (void)SetEvent(rs_thread->hEventMain);
+        (void)ResetEvent(rs_thread->hEventThread);
         (void)NtYieldExecution();
     } while(rs_thread->run);
     return 0;
@@ -40,15 +38,14 @@ static BOOL NtResumeSuspendThreadHybrid(const char* test_name, BOOL suspend)
 {
     ASSERT_HEADER;
 
-    HANDLE hMutex = CreateMutexA(NULL, FALSE, "mutex");
-    HANDLE hEventThread = CreateEventA(NULL, FALSE, FALSE, "event");
-    (void)WaitForSingleObject(hMutex, INFINITE);
+    HANDLE hEventMain = CreateEventA(NULL, TRUE, TRUE, "main");
+    HANDLE hEventThread = CreateEventA(NULL, TRUE, FALSE, "thread");
 
     rs_thread_struct rs_thread = {
         .run = TRUE,
         .counter = 0,
         .hEventThread = hEventThread,
-        .hMutex = hMutex
+        .hEventMain = hEventMain
     };
     ULONG old_suspend_count;
     ULONG counter = 0;
@@ -76,28 +73,28 @@ static BOOL NtResumeSuspendThreadHybrid(const char* test_name, BOOL suspend)
         ASSERT_FOOTER(test_name);
     }
 
-    (void)ReleaseMutex(hMutex);
     // Let other thread to run
     (void)SetEvent(hEventThread);
+    (void)ResetEvent(hEventMain);
     (void)NtYieldExecution();
 
     if (suspend) {
 
-        result_wait = WaitForSingleObject(hMutex, INFINITE);
-        GEN_CHECK(result_wait, WAIT_OBJECT_0, "result_wait");
+        result_wait = WaitForSingleObject(hEventMain, 1);
+        GEN_CHECK(result_wait, WAIT_TIMEOUT, "result_wait");
         // Check suspended state of the thread
         GEN_CHECK(rs_thread.counter, counter, "counter");
         // Resume the thread
         result = NtResumeThread(hThread, &old_suspend_count);
         GEN_CHECK(result, STATUS_SUCCESS, "result");
         GEN_CHECK(old_suspend_count, 1, "old_suspend_count");
-        (void)ReleaseMutex(hMutex);
 
         // Let other thread to run
         (void)SetEvent(hEventThread);
+        (void)ResetEvent(hEventMain);
         (void)NtYieldExecution();
 
-        result_wait = WaitForSingleObject(hMutex, INFINITE);
+        result_wait = WaitForSingleObject(hEventMain, 1);
         GEN_CHECK(result_wait, WAIT_OBJECT_0, "result_wait");
         // Check running state of the thread
         counter++;
@@ -107,22 +104,20 @@ static BOOL NtResumeSuspendThreadHybrid(const char* test_name, BOOL suspend)
         result = NtResumeThread(hThread, &old_suspend_count);
         GEN_CHECK(result, STATUS_SUCCESS, "result");
         GEN_CHECK(old_suspend_count, 0, "old_suspend_count");
-        (void)ReleaseMutex(hMutex);
 
         // Let other thread to run
         (void)SetEvent(hEventThread);
+        (void)ResetEvent(hEventMain);
         (void)NtYieldExecution();
 
-        result_wait = WaitForSingleObject(hMutex, INFINITE);
+        result_wait = WaitForSingleObject(hEventMain, 1);
         GEN_CHECK(result_wait, WAIT_OBJECT_0, "result_wait");
         // Making sure the thread is still running
         counter++;
         GEN_CHECK(rs_thread.counter, counter, "counter");
-
-        (void)ReleaseMutex(hMutex);
     }
     else {
-        result_wait = WaitForSingleObject(hMutex, INFINITE);
+        result_wait = WaitForSingleObject(hEventMain, 1);
         GEN_CHECK(result_wait, WAIT_OBJECT_0, "result_wait");
         // Check running state of the thread
         counter++;
@@ -131,72 +126,71 @@ static BOOL NtResumeSuspendThreadHybrid(const char* test_name, BOOL suspend)
         result = NtSuspendThread(hThread, &old_suspend_count);
         GEN_CHECK(result, STATUS_SUCCESS, "result");
         GEN_CHECK(old_suspend_count, 0, "old_suspend_count");
-        (void)ReleaseMutex(hMutex);
 
         // Let other thread to run (which in this case shouldn't be running)
         (void)SetEvent(hEventThread);
+        (void)ResetEvent(hEventMain);
         (void)NtYieldExecution();
 
-        result_wait = WaitForSingleObject(hMutex, INFINITE);
-        GEN_CHECK(result_wait, WAIT_OBJECT_0, "result_wait");
+        result_wait = WaitForSingleObject(hEventMain, 1);
+        GEN_CHECK(result_wait, WAIT_TIMEOUT, "result_wait");
         // Check suspended state of the thread
         GEN_CHECK(rs_thread.counter, counter, "counter");
         // Suspend the suspended thread again for suspend count check
         result = NtSuspendThread(hThread, &old_suspend_count);
         GEN_CHECK(result, STATUS_SUCCESS, "result");
         GEN_CHECK(old_suspend_count, 1, "old_suspend_count");
-        (void)ReleaseMutex(hMutex);
 
         // Let other thread to run (which in this case shouldn't be running)
         (void)SetEvent(hEventThread);
+        (void)ResetEvent(hEventMain);
         (void)NtYieldExecution();
 
-        result_wait = WaitForSingleObject(hMutex, INFINITE);
-        GEN_CHECK(result_wait, WAIT_OBJECT_0, "result_wait");
+        result_wait = WaitForSingleObject(hEventMain, 1);
+        GEN_CHECK(result_wait, WAIT_TIMEOUT, "result_wait");
         // Check suspended state of the thread
         GEN_CHECK(rs_thread.counter, counter, "counter");
         // Resume the suspend thread but it is remain suspended
         result = NtResumeThread(hThread, &old_suspend_count);
         GEN_CHECK(result, STATUS_SUCCESS, "result");
         GEN_CHECK(old_suspend_count, 2, "old_suspend_count");
-        (void)ReleaseMutex(hMutex);
 
         // Let other thread to run (which in this case shouldn't be running)
         (void)SetEvent(hEventThread);
+        (void)ResetEvent(hEventMain);
         (void)NtYieldExecution();
 
-        result_wait = WaitForSingleObject(hMutex, INFINITE);
-        GEN_CHECK(result_wait, WAIT_OBJECT_0, "result_wait");
+        result_wait = WaitForSingleObject(hEventMain, 1);
+        GEN_CHECK(result_wait, WAIT_TIMEOUT, "result_wait");
         // Check suspended state of the thread
         GEN_CHECK(rs_thread.counter, counter, "counter");
         // Resume the suspended thread
         result = NtResumeThread(hThread, &old_suspend_count);
         GEN_CHECK(result, STATUS_SUCCESS, "result");
         GEN_CHECK(old_suspend_count, 1, "old_suspend_count");
-        (void)ReleaseMutex(hMutex);
 
         // Let other thread to run
         (void)SetEvent(hEventThread);
+        (void)ResetEvent(hEventMain);
         (void)NtYieldExecution();
 
-        result_wait = WaitForSingleObject(hMutex, INFINITE);
+        result_wait = WaitForSingleObject(hEventMain, 1);
         GEN_CHECK(result_wait, WAIT_OBJECT_0, "result_wait");
         // Making sure the thread is still running
         counter++;
         GEN_CHECK(rs_thread.counter, counter, "counter");
-
-        (void)ReleaseMutex(hMutex);
     }
 
     // End of test, tell the other thread to terminate
     rs_thread.run = FALSE;
     (void)SetEvent(hEventThread);
+    (void)ResetEvent(hEventMain);
     (void)NtYieldExecution();
 
     // Perform the clean up process requirement
     (void)CloseHandle(hThread);
     (void)CloseHandle(hEventThread);
-    (void)CloseHandle(hMutex);
+    (void)CloseHandle(hEventMain);
     ASSERT_FOOTER(test_name);
 }
 #undef THREAD_TEST_FAILED
