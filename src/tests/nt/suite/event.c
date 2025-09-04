9c305666d92f78119b8bedcdcb8801ed14e89e7f
RadWolfie <RadWolfie@users.noreply.github.com>
2025-09-03 21:29:32 -0500

    create event stub for Nt APIs

---
+++ src/tests/nt/suite/event.c
@@ -0,0 +1,28 @@
+#include <xboxkrnl/xboxkrnl.h>
+
+#include "util/output.h"
+
+TEST_FUNC(NtClearEvent)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(NtCreateEvent)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(NtPulseEvent)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(NtQueryEvent)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(NtSetEvent)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
