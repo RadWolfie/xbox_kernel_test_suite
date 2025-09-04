de9d31c1b94495e427465d4a21724f0da1f4799e
RadWolfie <RadWolfie@users.noreply.github.com>
2025-09-03 21:30:08 -0500

    create event stub for Ke APIs

--- src/tests/ke/KePulseEvent.c
+++
@@ -1,8 +0,0 @@
-#include <xboxkrnl/xboxkrnl.h>
-
-#include "util/output.h"
-
-TEST_FUNC(KePulseEvent)
-{
-    /* FIXME: This is a stub! implement this function! */
-}
---
+++ src/tests/ke/suite/event.c
@@ -0,0 +1,23 @@
+#include <xboxkrnl/xboxkrnl.h>
+
+#include "util/output.h"
+
+TEST_FUNC(KeInitializeEvent)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KePulseEvent)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeResetEvent)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
+TEST_FUNC(KeSetEvent)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
