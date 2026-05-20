0868fcfdf74adcb54902f09b29e0c364c1a30b0b
PatrickvL <PatrickvL@users.noreply.github.com>
2026-05-20 10:00:25 +0200

    fix macro argument parenthesization in defines.h

--- src/assertions/defines.h
+++ src/assertions/defines.h
@@ -11,37 +11,37 @@
     return test_passed;
 
 #define GEN_CHECK_EX(check_var, expected_var, varname, func_line) \
-    if(check_var != expected_var) { \
+    if((check_var) != (expected_var)) { \
         print( \
             ((sizeof(check_var) > 4) ? \
             "  ERROR(line %d): Expected %s = 0x%llx, Got = 0x%llx" : \
             "  ERROR(line %d): Expected %s = 0x%x, Got = 0x%x") \
-            , func_line, varname, expected_var, check_var \
+            , func_line, varname, (expected_var), (check_var) \
         ); \
         TEST_FAILED(); \
     }
 #define GEN_CHECK(check_var, expected_var, varname) GEN_CHECK_EX(check_var, expected_var, varname, __LINE__)
 
 #define GEN_CHECK_RANGE_EX(check_var, expected_var, size, varname, func_line) \
-    if(check_var < expected_var || check_var > expected_var + size) { \
+    if((check_var) < (expected_var) || (check_var) > (expected_var) + (size)) { \
         print( \
             ((sizeof(check_var) > 4) ? \
             "  ERROR(line %d): Expected range %s = 0x%x-0x%llx, Got = 0x%llx" : \
             "  ERROR(line %d): Expected range %s = 0x%x-0x%x, Got = 0x%x") \
-            , func_line, varname, expected_var, expected_var + size, check_var \
+            , func_line, varname, (expected_var), (expected_var) + (size), (check_var) \
         ); \
         TEST_FAILED(); \
     }
 #define GEN_CHECK_RANGE(check_var, expected_var, size, varname) GEN_CHECK_RANGE_EX(check_var, expected_var, size, varname, __LINE__)
 
 #define GEN_CHECK_ARRAY_EX(check_var, expected_var, size, varname, func_line) \
-    for (unsigned i = 0; i < size; i++) { \
-        if (check_var[i] != expected_var[i]) { \
+    for (unsigned i = 0; i < (size); i++) { \
+        if ((check_var)[i] != (expected_var)[i]) { \
             print( \
-                ((sizeof(check_var[i]) > 4) ? \
+                ((sizeof((check_var)[i]) > 4) ? \
                 "  ERROR(line %d): Expected array %s[%u] = 0x%llx, Got = 0x%llx" : \
                 "  ERROR(line %d): Expected array %s[%u] = 0x%x, Got = 0x%x") \
-                , func_line, varname, i, expected_var[i], check_var[i] \
+                , func_line, varname, i, (expected_var)[i], (check_var)[i] \
             ); \
             TEST_FAILED(); \
         } \
@@ -49,13 +49,13 @@
 #define GEN_CHECK_ARRAY(check_var, expected_var, size, varname) GEN_CHECK_ARRAY_EX(check_var, expected_var, size, varname, __LINE__)
 
 #define GEN_CHECK_ARRAY_MEMBER_EX(var, m_check, m_expected, size, varname, func_line) \
-    for (unsigned i = 0; i < size; i++) { \
-        if (var[i].m_check != var[i].m_expected) { \
+    for (unsigned i = 0; i < (size); i++) { \
+        if ((var)[i].m_check != (var)[i].m_expected) { \
             print( \
-                ((sizeof(var[i].m_check) > 4) ? \
+                ((sizeof((var)[i].m_check) > 4) ? \
                 "  ERROR(line %d): Expected array %s[%u].%s = 0x%llx, Got %s[%u].%s = 0x%llx" : \
                 "  ERROR(line %d): Expected array %s[%u].%s = 0x%x, Got %s[%u].%s = 0x%x") \
-                , func_line, varname, i, #m_expected, var[i].m_expected, varname, i, #m_check, var[i].m_check \
+                , func_line, varname, i, #m_expected, (var)[i].m_expected, varname, i, #m_check, (var)[i].m_check \
             ); \
             TEST_FAILED(); \
         } \
