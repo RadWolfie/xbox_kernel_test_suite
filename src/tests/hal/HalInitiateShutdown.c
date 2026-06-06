4a2de7dbe23209bf167a2d82dbb1e40fb2cdff7a
PatrickvL <PatrickvL@users.noreply.github.com>
2026-06-06 03:29:22 -0500

    Skip Hal tests: power
    - HalInitiateShutdown
    - HalReturnToFirmware

--- src/tests/hal/HalInitiateShutdown.c
+++ src/tests/hal/HalInitiateShutdown.c
@@ -1,8 +1,14 @@
 #include <xboxkrnl/xboxkrnl.h>
 
 #include "util/output.h"
+#include "assertions/defines.h"
 
 TEST_FUNC(HalInitiateShutdown)
 {
-    /* FIXME: This is a stub! implement this function! */
+    TEST_BEGIN();
+
+    // HalInitiateShutdown will shut down the system
+    // TODO: Need a config variable to determine allow to test or not
+
+    TEST_SKIP_END("will shut down system");
 }
--- src/tests/hal/HalReturnToFirmware.c
+++ src/tests/hal/HalReturnToFirmware.c
@@ -1,8 +1,14 @@
 #include <xboxkrnl/xboxkrnl.h>
 
 #include "util/output.h"
+#include "assertions/defines.h"
 
 TEST_FUNC(HalReturnToFirmware)
 {
-    /* FIXME: This is a stub! implement this function! */
+    TEST_BEGIN();
+
+    // HalReturnToFirmware will reboot/shutdown
+    // TODO: Need a config variable to determine allow to test or not
+
+    TEST_SKIP_END("will shut down, or reboot, system");
 }
