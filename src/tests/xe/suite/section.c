ff040206a97ffa126f3b1bc0835e9c2533cc5300
RadWolfie <RadWolfie@users.noreply.github.com>
2026-06-22 08:56:41 -0500

    create section stub for Xe APIs

--- src/tests/xe/suite/section.c
+++ src/tests/xe/suite/section.c
@@ -2,6 +2,11 @@
 
 #include "util/output.h"
 
+TEST_FUNC(XeLoadSection)
+{
+    /* FIXME: This is a stub! implement this function! */
+}
+
 TEST_FUNC(XeUnloadSection)
 {
     /* FIXME: This is a stub! implement this function! */
